ec2f9c27db227f645ef437f65ac07b97757e7a87
ReasonableM <alesilenok@yandex.ru>
2025-11-25 23:43:11 +0300

    Связал бэк с фронтендом, добавив разрешения

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import engine, Base
 from . import models
@@ -9,6 +10,15 @@ Base.metadata.create_all(bind=engine)
 
 # Создаём экземпляр приложения FastAPI
 app = FastAPI(title="MyBooks API", version="1.0.0")
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Подключаем роутер книг
 app.include_router(books.router)
